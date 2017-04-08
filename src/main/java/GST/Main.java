4dbdd041ef8106ea67633128b289d7faf4ce2ba6
pwrliang <pwrliang@gmail.com>
2017-04-08 09:22:36 +0800

    delete redundant code

--- src/main/java/GST/Main.java
+++ src/main/java/GST/Main.java
@@ -2,13 +2,18 @@ package GST;
 
 import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryo.serializers.FieldSerializer;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.LocatedFileStatus;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.RemoteIterator;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 import java.io.*;
+import java.net.URI;
 import java.util.*;
 
 import org.apache.spark.api.java.function.FlatMapFunction;
@@ -47,40 +52,65 @@ public class Main {
         }
     }
 
+    public static String readFile(String url, char terminator) throws IOException {
+        Path path = new Path(url);
+        Configuration conf = new Configuration();
+        conf.set("fs.defaultFS", url);//hdfs://master:9000
+        FileSystem fileSystem = FileSystem.get(conf);
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileSystem.open(path)));
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            sb.append(line);
+        }
+        sb.append(terminator);
+        return sb.toString();
+    }
+
+
+    public static List<String> listFiles(String url) throws IOException {
+        Path path = new Path(url);
+        URI uri = path.toUri();
+        String hdfsPath = String.format("%s://%s:%d", uri.getScheme(), uri.getHost(), uri.getPort());
+        Configuration conf = new Configuration();
+        conf.set("fs.defaultFS", hdfsPath);
+
+        FileSystem fileSystem = FileSystem.get(conf);
+        RemoteIterator<LocatedFileStatus> files = fileSystem.listFiles(path, true);
+        List<String> pathList = new ArrayList<String>();
+        while (files.hasNext()) {
+            LocatedFileStatus file = files.next();
+            pathList.add(file.getPath().toString());
+        }
+        return pathList;
+    }
+
     public static void main(String[] args) throws IOException, InterruptedException {
+        long start = System.currentTimeMillis();
         final String inputURL = args[0];
         final String outputURL = args[1];
         String param = "default";
         if (args.length == 3)
             param = args[2];
         SparkConf conf = new SparkConf().
-                setAppName(new Path(inputURL).getName() + " Fm:" + param).setMaster("local");
+                setAppName(new Path(inputURL).getName() + " Fm:" + param);
         conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer");
         conf.set("spark.kryo.registrator", ClassRegistrator.class.getName());
         conf.set("spark.kryoserializer.buffer.max", "2047");
+        final ERA era = new ERA();
         final JavaSparkContext sc = new JavaSparkContext(conf);
         final Map<Character, String> terminatorFilename = new HashMap<Character, String>();//终结符:文件名
-
+        List<String> pathList = listFiles(inputURL);
+        List<String> S = new ArrayList<String>();
         //开始读取文本文件
         //key filename value content
-        JavaPairRDD<String, String> inputDataRDD = sc.wholeTextFiles(inputURL).cache();//read whole folder
-        final Map<String,Character> filenameTerminator = new HashMap<String, Character>();
-        final ERA era = new ERA();
-        List<String> filenameList =  inputDataRDD.keys().collect();
-        for (String filename : filenameList) {
-            Character terminator = era.nextTerminator();
-            filenameTerminator.put(filename,terminator);
-            terminatorFilename.put(terminator, filename);
-        }
 
-        inputDataRDD = inputDataRDD.mapToPair(new PairFunction<Tuple2<String, String>, String, String>() {
-            public Tuple2<String, String> call(Tuple2<String, String> input) throws Exception {
-                String filename = input._1;
-                Character terminator = filenameTerminator.get(filename);
-                return new Tuple2<String, String>(filename,input._2+terminator);
-            }
-        });
-        List<String> S = inputDataRDD.values().collect();
+        for (String path : pathList) {
+            char terminator = era.nextTerminator();
+            String content = readFile(path, terminator);
+            S.add(content);
+            terminatorFilename.put(terminator, new Path(path).getName());
+        }
 
         int lengthForAll = 0;
         for (String s : S)
@@ -91,8 +121,15 @@ public class Main {
             Fm = FmSelector(lengthForAll);
         else
             Fm = Integer.valueOf(param);
+        System.out.println("read:" + (System.currentTimeMillis() - start));
+        start = System.currentTimeMillis();
         Set<Character> alphabet = ERA.getAlphabet(S);//扫描串获得字母表
-        Set<Set<String>> setOfVirtualTrees = era.verticalPartitioning(S, alphabet, Fm);//开始垂直分区
+        System.out.println("scan alphabet:" + (System.currentTimeMillis() - start));
+        start = System.currentTimeMillis();
+        Set<Set<String>> setOfVirtualTrees = era.verticalPartitioningTest(S, alphabet, Fm);//开始垂直分区
+        System.out.println("vertical partition:" + (System.currentTimeMillis() - start));
+        start = System.currentTimeMillis();
+        System.gc();
         //分配任务
         final Broadcast<List<String>> broadcastStringList = sc.broadcast(S);
         final Broadcast<Map<Character, String>> broadcasterTerminatorFilename = sc.broadcast(terminatorFilename);
@@ -117,5 +154,6 @@ public class Main {
             }
         });
         resultRDD.saveAsTextFile(outputURL);
+        System.out.println("other procedure:" + (System.currentTimeMillis() - start));
     }
 }
