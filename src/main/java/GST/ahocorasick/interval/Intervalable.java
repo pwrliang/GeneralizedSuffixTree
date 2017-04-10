c1a3b1516fe82d99479043229324ac57613dd112
pwrliang@gmail.com <geng_93418>
2017-04-10 16:01:23 +0800

    add new AC algorithm

---
+++ src/main/java/GST/ahocorasick/interval/Intervalable.java
@@ -0,0 +1,11 @@
+package GST.ahocorasick.interval;
+
+public interface Intervalable extends Comparable {
+
+    public int getStart();
+
+    public int getEnd();
+
+    public int size();
+
+}
---
+++ src/main/java/GST/ahocorasick/trie/handler/DefaultEmitHandler.java
@@ -0,0 +1,18 @@
+package GST.ahocorasick.trie.handler;
+import GST.ahocorasick.trie.Emit;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DefaultEmitHandler implements EmitHandler {
+
+    private final List<Emit> emits = new ArrayList<>();
+
+    @Override
+    public void emit(final Emit emit) {
+        this.emits.add(emit);
+    }
+
+    public List<Emit> getEmits() {
+        return this.emits;
+    }
+}
